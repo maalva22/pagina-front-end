64cd23c2718d1f5506d18249b452d0a5ea19e73e
David Morantes <80722555+PhantomBlack219@users.noreply.github.com>
2022-04-11 23:32:54 -0500

    actualizaciones

--- Boca/paginas/urls.py
+++ Boca/paginas/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+     path('contest/', views.contest, name='contest'),
 ]
