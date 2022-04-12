cf90b3cf75581f54af095a1a8470e49ec61cc9ae
David Morantes <80722555+PhantomBlack219@users.noreply.github.com>
2022-04-12 15:29:41 -0500

    navbar principal funcional, login funcional,

--- Boca/paginas/admin.py
+++ Boca/paginas/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import contest
+from .models import contest,option
 
 admin.site.register(contest)
+admin.site.register(option)
